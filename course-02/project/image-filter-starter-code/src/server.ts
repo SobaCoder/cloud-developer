394e4fa35e94414474a02ced5e78d6a165841c20
Anwesha Bhattacharya <banwesha@amazon.com>
2020-06-15 18:55:41 -0700

    Implement Image filtering in GET method of Node.js

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,7 +30,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+
+  app.get( "/filteredimage/", async ( req, res ) => {
+      var image_url = req.query.image_url;
   
+      if (!image_url) {
+        return res.status(400).send(`image_url is required`);
+      }
+      let filteredpath = await filterImageFromURL(image_url);
+      res.sendfile(filteredpath);
+      setTimeout(function(){
+        deleteLocalFiles([filteredpath]);
+      }, 1000, "tmp file " + filteredpath);
+  });
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
